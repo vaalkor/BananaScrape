b36e441fa896d60a45f9f104c1a0ae00ce08038d
Etstansfield <etstansfield@hotmail.com>
2020-10-12 21:00:39 +0100

    Change Download Location
    Added two new cmd options:
    userDlLocation - changes the download locations in the browser(s)
    searchForInstall - if true and download is true and userDlLocation is empty - will search your harddrive(s) for the hl2dm install location - if found it will change the download location to the maps folder there

--- Drivers.cs
+++ Drivers.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using System;
 using System.IO;
 using System.Reflection;
 
@@ -7,16 +8,21 @@ namespace BananaScrape
 {
     public static class Drivers
     {
-        public static ChromeDriver CreateChromeDriver()
+        public static ChromeDriver CreateChromeDriver(string installLocation = "")
         {
             //Disable loading of images.
             ChromeOptions options = new ChromeOptions();
             options.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
 
+            if (!String.IsNullOrEmpty(installLocation))
+            {
+                options.AddUserProfilePreference("download.default_directory", $"{installLocation}");
+            }
+
             return new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
         }
 
-        public static FirefoxDriver CreateFirefoxDriver()
+        public static FirefoxDriver CreateFirefoxDriver(string installLocation = "")
         {
 
             FirefoxOptions options = new FirefoxOptions();
@@ -28,6 +34,13 @@ namespace BananaScrape
             FirefoxDriverService geckoService = FirefoxDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             geckoService.Host = "::1";
             options.AcceptInsecureCertificates = true;
+
+            if (!String.IsNullOrEmpty(installLocation))
+            {
+                options.SetPreference("browser.download.folderList", 2);    // Tells Firefox we want to set the download location to a user defined one
+                options.SetPreference("browser.download.dir", $"{installLocation}");
+            }
+
             return new FirefoxDriver(geckoService, options);
         }
     }
